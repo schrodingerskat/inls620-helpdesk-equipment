37e12ca27bdf063b5fc811b6d62350718dda68c5
Katherine Shaw <shaw.comma.katherine@gmail.com>
2016-12-13 08:24:50 -0500

    add reservation pages

--- server.py
+++ server.py
@@ -1,14 +1,23 @@
-from flask import Flask, render_template, make_response, redirect, send_from_directory
-from flask.ext.restful import Api, Resource, reqparse, abort
-
 import json
 import string
 import random
 from datetime import datetime
 
+from flask import Flask, render_template, make_response, redirect, send_from_directory
+from flask.ext.restful import Api, Resource, reqparse, abort
+
+
+
+def date(s):
+    return datetime.strptime(s, "%Y-%m-%d").date()
+
 # Load data from JSON "database"
 with open('data.jsonld') as data:
     data = json.load(data)
+    for reservation in data['reservations']:
+        for date_string in ['startDate', 'endDate']:
+            data['reservations'][reservation][date_string] = \
+                date(data['reservations'][reservation][date_string])
 
 
 # Generate a unique ID for a new item or reservation.
@@ -53,6 +62,22 @@ def filter_and_sort_equipment(query='', sort_by='time'):
 
 
 # Given the data for an equipment record, generate an HTML representation.
+def render_reservation_as_html(reservation_record, equipment_record):
+    return render_template(
+        'reservation.html',
+        reservation=reservation_record,
+        item=equipment_record)
+
+
+# Given the data for a list of equipment, generate an HTML representation
+# of that list.
+def render_reservation_list_as_html(reservations, equipment):
+    return render_template(
+        'reservation-calendar.html',
+        reservations=reservations,
+        equipment=equipment)
+
+# Given the data for an equipment record, generate an HTML representation.
 def render_equipitem_as_html(equipment_record):
     return render_template(
         'equipment-item.html',
@@ -64,8 +89,7 @@ def render_equipitem_as_html(equipment_record):
 def render_equipment_list_as_html(equipment):
     return render_template(
         'equipment-list.html',
-        equipment=equipment
-        )
+        equipment=equipment)
 
 
 # Raises an error if the string x is empty (has zero length).
@@ -73,7 +97,6 @@ def nonempty_string(x):
     if len(x) == 0:
         raise ValueError('string is empty')
     return str(x)
-
 
 
 ###########################################################
@@ -170,6 +193,88 @@ class HelpRequestListAsJSON(Resource):
     def get(self):
         return data
 
+###########################################################
+##                RESERVATIONS : Parsers                 ##
+###########################################################
+
+# Specify the data necessary to create a new reservation.
+# Required Attributes:
+# 'requestedBy', 'requestedItem', 'startDate', 'endDate'
+new_reservation_parser = reqparse.RequestParser()
+for arg in ['requestedBy', 'requestedItem']:
+    new_reservation_parser.add_argument(
+        arg, type=nonempty_string, required=True,
+        help="'{}' is a required value".format(arg))
+for arg in ['startDate', 'endDate']:
+    new_reservation_parser.add_argument(
+        arg, type=date, required=True,
+        help="'{}' is a required value".format(arg))
+
+# Specify the data necessary to update an existing item record.
+# Only the start and end dates can be updated
+update_reservation_parser = reqparse.RequestParser()
+update_reservation_parser.add_argument(
+    'startDate', type=date)
+update_reservation_parser.add_argument(
+    'endDate', type=date)
+
+
+###########################################################
+##             RESERVATIONS : Full Calendar              ##
+###########################################################
+
+# Define our help request list resource.
+class ReservationList(Resource):
+    # Respond with an HTML representation of the help request list, after
+    # applying any filtering and sorting parameters.
+    def get(self):
+        return make_response(
+            render_reservation_list_as_html(
+                data['reservations'], data['equipment']), 200)
+
+    # Add a new help request to the list, and respond with an HTML
+    # representation of the updated list.
+    # TODO
+    def post(self):
+        reservation = new_reservation_parser.parse_args()
+        reservation_id = generate_id()
+        reservation['@id'] = 'reservation/' + reservation_id
+        reservation['@type'] = 'helpdesk:EquipmentReservation'
+        data['reservations'][reservation_id] = reservation
+        return make_response(
+            render_reservation_list_as_html(
+                data['reservations'], data['equipment']), 201)
+
+
+
+###########################################################
+##         RESERVATIONS : Individual Reservation         ##
+###########################################################
+
+class Reservation(Resource):
+
+    # If a reservation with the specified ID does not exist,
+    # respond with a 404, otherwise respond with an HTML representation.
+    def get(self, reservation_id):
+        error_if_reservation_not_found(reservation_id)
+        this_reservation = data['reservations'][reservation_id]
+        return make_response(
+            render_reservation_as_html(
+                this_reservation,
+                data['equipment'][this_reservation['requestedItem']]), 200)
+
+    # If a help request with the specified ID does not exist,
+    # respond with a 404, otherwise update the help request and respond
+    # with the updated HTML representation.
+    def patch(self, reservation_id):
+        error_if_reservation_not_found(reservation_id)
+        reservation = data['reservations'][reservation_id]
+        update = update_reservation_parser.parse_args()
+        reservation['startDate'] = update['startDate']
+        reservation['endDate'] = update['endDate']
+        return make_response(
+            render_reservation_as_html(reservation,
+                data['equipment'][reservation['requestedItem']]), 200)
 
 # Assign URL paths to our resources.
 app = Flask(__name__)
@@ -178,6 +283,8 @@ api.add_resource(EquipmentList, '/equipment')
 api.add_resource(HelpRequestListAsJSON, '/requests.json')
 api.add_resource(EquipmentItem, '/equipment/<string:equipment_id>')
 api.add_resource(HelpRequestAsJSON, '/request/<string:helprequest_id>.json')
+api.add_resource(ReservationList, '/reservations')
+api.add_resource(Reservation, '/reservation/<string:reservation_id>')
 
 
 # Redirect from the index to the list of help requests.
